fc8053671171f9dfb00192ecadecc5288bade9e2
shaozifan521 <137164891@qq.com>
2020-12-30 14:33:58 +0800

    安装parcel,将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="g-button">{{count}}</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: '按钮1'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .g-button {
+    height: var(--button-height);
+    background-color: var(--button-bg);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
