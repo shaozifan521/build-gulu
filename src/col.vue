a6d0c4bbabd85c9adec0906ed8abd7a6c3000d8e
shaozifan521 <137164891@qq.com>
2021-01-06 16:15:36 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, `offset-${offset}`]" :style="{ 'padding-left': gutter / 2 + 'px', 'padding-right': gutter / 2 + 'px' }">
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,11 +11,14 @@ export default {
   props: {
     span: {
       type: [String, Number]
+    },
+    offset: {
+      type: [String, Number]
     }
   },
   data () {
     return {
-
+      gutter: 0
     }
   }
 }
@@ -21,13 +26,19 @@ export default {
 
 <style scoped lang="scss">
   .col {
-    height: 100px;
+    // height: 100px;
     width: 50%; // 列的宽度固定50%，为什么三列会自动分布？因为 flex-wrap 属性默认是不换行，改成 flex-wrap: wrap 就会换行显示了
-    background-color: gray;
-    border: 1px solid pink;
+    // background-color: gray;
+    // border: 1px solid pink;
+    // for循环
     @for $i from 1 through 24 {
       &.col-#{$i} {
         width: ($i / 24) * 100%;
+      }
+    }
+    @for $i from 1 through 24 {
+      &.offset-#{$i} {
+        margin-left: ($i / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{ 'margin-letf': -gutter / 2 + 'px', 'margin-right': -gutter / 2 + 'px' }">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    gutter: {
+      type: [String, Number]
+    }
+  },
   data () {
     return {
 
     }
-  }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  },
 }
 </script>
 
