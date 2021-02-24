b6502647195838c9e4c45260aff58c633d3a9c4f
邵宏博 <137164891@qq.com>
2021-02-24 15:48:33 +0800

    collapse 组件基本功能和样式完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="collapse-item">
+    <div class="title" @click="content = !content">
+      {{title}}
+    </div>
+    <div class="content" v-if="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluCollapseItem',
+  props:{
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      content: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .collapse-item {
+    border-radius: 4px;
+    .title {
+      border: 1px solid #ddd;
+      margin-left: -1px;
+      margin-right: -1px;
+      margin-top: -1px;
+      padding: 8px;
+      min-height: 32px;
+      display: flex;
+      align-items: center;
+    }
+    .content {
+      padding: 8px;
+    }
+    &:first-child {
+      .title {
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+    }
+    // 当最后一个元素的title元素变成最后一个元素的时候
+    &:last-child {
+       .title:last-child {
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+        margin-bottom: -1px;
+      }
+    }
+  }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluCollapse',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .collapse {
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+</style>
